4356e32e579a4e732c7126c85064ec18d6e573dc
Xyphnos <32095355+Xyphnos@users.noreply.github.com>
2019-03-21 13:10:37 +0200

    Testing command

---
+++ build/precache-manifest.a3233cbb742fd4f948e50e363cd60486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b53f97caef884a1eccd6",
+    "url": "./static/js/main.1bad8308.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "./static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "b53f97caef884a1eccd6",
+    "url": "./static/css/main.0bb690a6.chunk.css"
+  },
+  {
+    "revision": "884866d71cd620acc52a7d1071132642",
+    "url": "./index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            picArray: [
+                {
+                    'title': 'Title 1',
+                    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
+                    'thumbnails': {
+                        w160: 'http://placekitten.com/160/161'
+                    },
+                    'filename': 'http://placekitten.com/2048/1920',
+                },
+                {
+                    'title': 'Title 2',
+                    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
+                    'thumbnails': {
+                        w160: 'http://placekitten.com/160/162'
+                    },
+                    'filename': 'http://placekitten.com/2041/1922',
+                },
+                {
+                    'title': 'Title 3',
+                    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
+                    'thumbnails': {
+                        w160: 'http://placekitten.com/160/164'
+                    },
+                    'filename': 'http://placekitten.com/2039/1920',
+                },
+            ],
+        }
+    }
+  render() {
+    return (
+          <table>
+              <tbody>
+              {this.state.picArray.map(function(item, key) {
+
+                  return (
+                      <tr key = {key}>
+                          <td>
+                              <img src={item.thumbnails.w160}/>
+                          </td>
+                          <td>
+                              <h3>{item.title}</h3>
+                              <p>{item.description}</p>
+                          </td>
+                          <td>
+                              <a href={item.filename}>View</a>
+                          </td>
+                      </tr>
+                  )
+
+              })}
+              </tbody>
+          </table>
+    );
+  }
+}
+
+export default App;
